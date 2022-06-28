afeaecef082e59f6338f674bc6f657f128058240
Raju Chandnani <Raju.Chandnani@silkroad.com>
2022-06-28 09:06:33 -0700

    test

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -8,6 +8,8 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /*----------------------TEST PIPELINE -------------------------------------------*/
+
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
@@ -19,16 +21,45 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
     });
 
 
-    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+    const devqa = pipeline.addStage(new MyPipelineAppStage(this, "devqa", {
       env: { account: "091444724237", region: "us-east-1" }
     }));
 
 
-    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
-    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+    devqa.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    devqa.addPost(new ManualApprovalStep('Manual approval before dev'));
 
-    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+    const devStage = pipeline.addStage(new MyPipelineAppStage(this, "dev", {
       env: { account: "091444724237", region: "us-east-1" }
     }));
+
+    /*****************End OF TEST PIPE LINE************************************************* */
+
+
+
+    // /*******************************PRODUCTION PIPE LINE************************************ */
+
+    // const prodpipeline = new CodePipeline(this, 'ProdPipeline', {
+    //   pipelineName: 'ProdPipeline',
+    //   synth: new ShellStep('Synth', {
+    //     input: CodePipelineSource.gitHub('rchandnani0615/ci-cd-aws-pipeline-demo', 'master'),  
+    //     commands: ['npm ci', 
+    //                'npm run build', 
+    //                'npx cdk synth']
+    //   })
+    // });
+
+
+    // const qastage = pipeline.addStage(new MyPipelineAppStage(this, "qa", {
+    //   env: { account: "091444724237", region: "us-east-1" }
+    // }));
+
+    // qastage.addPost(new ManualApprovalStep('Manual approval before prod'));
+
+    // const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+    //   env: { account: "091444724237", region: "us-east-1" }
+    // }));
+
+    // /********************************END PRODUCTION PIPE LINE********************************* */
   }
 }
